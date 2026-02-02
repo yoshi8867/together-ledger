e9aa492c14c2a811809bf6943f4ace53bd39ce5c
yoshi <yoshi.8867.0311@gmail.com>
2026-02-03 00:33:50 +0900

    DetailScreen 추가 완료: 하지만 나중에는 아이템 클릭 시 곧바로 수정 페이지로 가는 게 간편할 것으로 보임.

---
+++ app/src/main/java/com/yoshi0311/togetherledger/ui/transaction/TransactionDetailsViewModel.kt
@@ -0,0 +1,42 @@
+package com.yoshi0311.togetherledger.ui.transaction
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yoshi0311.togetherledger.data.TransactionsRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class TransactionDetailsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val transactionsRepository: TransactionsRepository,
+) : ViewModel() {
+
+    private val transactionId: Int = checkNotNull(savedStateHandle[TransactionDetailsDestination.transactionIdArg])
+
+    val uiState: StateFlow<TransactionDetailsUiState> =
+        transactionsRepository.getTransactionStream(transactionId)
+            .filterNotNull()
+            .map {
+                TransactionDetailsUiState(transactionDetails = it.toTransactionDetails())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = TransactionDetailsUiState()
+            )
+
+    suspend fun deleteItem() {
+        transactionsRepository.deleteTransaction(uiState.value.transactionDetails.toTransaction())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+data class TransactionDetailsUiState(
+    val transactionDetails: TransactionDetails = TransactionDetails(),
+)
